ac5c34e998c83159926252ccc6089ef34a658e74
xjunz <zxj19994@gmail.com>
2021-06-20 22:12:18 +0800

    Initial commit

---
+++ app/src/main/java/top/xjunz/automator/MainActivity.kt
@@ -0,0 +1,13 @@
+package top.xjunz.automator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *    @author xjunz 2021/6/20 21:05
+ */
+class MainActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
